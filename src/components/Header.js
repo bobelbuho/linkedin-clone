1b861dca93e091fd7e20674ea0e7e66725c68b1a
bobelbuho <bob.vaz@hotmail.fr>
2023-04-05 14:26:30 +0200

    1h04

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,19 @@ const Header = (props) => {
 };
 
 const Container = styled.div`
-
+background-color: white;
+border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+left: 0;
+padding: 0 24px;
+position: fixed;
+top: 0;
+width: 100%;
+z-index: 100;
 `;
 const Content = styled.div`
-
+display: flex;
+align-items: center;
+margin: 0 auto;
+min-height: 100%;
 `;
 export default Header;
